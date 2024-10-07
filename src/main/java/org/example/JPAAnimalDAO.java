38b701ae52a96a10ebc28bd31e5fb1eb54a375b5
Minden Áron <you@inf.unideb.hu>
2024-10-07 13:30:51 +0200

    DAO ready

--- src/main/java/org/example/JPAAnimalDAO.java
+++ src/main/java/org/example/JPAAnimalDAO.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class JPAAnimalDAO implements AnimalDAO{
@@ -19,16 +20,23 @@ public class JPAAnimalDAO implements AnimalDAO{
 
     @Override
     public List<Animal> getAllAnimals() {
-        return List.of();
+        TypedQuery<Animal> query = entityManager.createQuery(
+                "SELECT a FROM Animal a", Animal.class);
+        List<Animal> animals = query.getResultList();
+        return animals;
     }
 
     @Override
     public void updateAnimal(Animal a) {
-
+        entityManager.getTransaction().begin();
+        entityManager.persist(a);
+        entityManager.getTransaction().commit();
     }
 
     @Override
     public void deleteAnimal(Animal a) {
-
+        entityManager.getTransaction().begin();
+        entityManager.remove(a);
+        entityManager.getTransaction().commit();
     }
 }
