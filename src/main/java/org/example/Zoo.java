b2983e03067f8533560f945404829ff6bf1d26e6
Minden Áron <you@inf.unideb.hu>
2024-10-14 13:19:11 +0200

    Zoo added2

--- src/main/java/org/example/Zoo.java
+++ src/main/java/org/example/Zoo.java
@@ -1,11 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Zoo {
     private int id;
     private String name;
-    private List<Animal> animals;
+    private List<Animal> animals = new ArrayList<>();
 
+    public Zoo(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
 }
